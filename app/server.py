dd69569d2930a71aee849479188405010adaef38
jianzhiw <49110948+jianzhiw@users.noreply.github.com>
2019-04-19 20:21:37 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -55,7 +55,7 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     prob = torch.max(learn.predict(img)[2])
-    return JSONResponse({'result': str(prediction), 'probability': str(prob)})
+    return JSONResponse({'result': str(prediction), 'probability': str(prob.item())})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
